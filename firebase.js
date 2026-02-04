33e80fab58c192e6d48c892b517723a826c159c3
SistemaIntegralDeGestion <juniorcede3002@gmail.com>
2026-02-03 20:30:56 -0500

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+// Tu configuración proporcionada
+const firebaseConfig = {
+  apiKey: "AIzaSyA5pUM_BycmKkbfhreDUrUH5CJEg_ykUW4",
+  authDomain: "proyectocoorporativo.firebaseapp.com",
+  projectId: "proyectocoorporativo",
+  storageBucket: "proyectocoorporativo.firebasestorage.app",
+  messagingSenderId: "152806986808",
+  appId: "1:152806986808:web:6944d32424e27dbf9e82cd"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
